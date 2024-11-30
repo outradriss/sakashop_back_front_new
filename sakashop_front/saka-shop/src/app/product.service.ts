115787d20cefe660c2657b7a176fa1f84d4c5bed
driss outrah <drissoutraah@gmail.com>
2024-11-30 10:12:12 +0100

    add front caisse / credit client .. with design

---
+++ sakashop_front/saka-shop/src/app/product.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from './models/product.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private apiUrl = 'http://localhost:8090/api/products'; // Remplacez par l'URL de votre backend
+
+  constructor(private http: HttpClient) {}
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(`${this.apiUrl}/list`);
+  }
+  saveProduct(product: Product): Observable<Product> {
+    debugger
+    return this.http.post<Product>(`${this.apiUrl}/save`, product);
+  }
+  
+  updateProduct(id: number, product: Product): Observable<Product> {
+    debugger
+    return this.http.put<Product>(`${this.apiUrl}/${id}`, product);
+  }
+  deleteProduct(id: number): Observable<string> {
+    return this.http.delete(`${this.apiUrl}/delete/${id}`, { responseType: 'text' });
+  }
+  
+
+}

39b824271d3b94a4a5635a9c4f576eaa2ac20f85
driss outrah <drissoutraah@gmail.com>
2024-12-02 00:46:21 +0100

    add desabled promo and design + backend

---
+++ sakashop_front/saka-shop/src/app/service/product-service/caisse-service/caisse.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../../../models/product.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CaisseService {
+  private apiUrl = 'http://localhost:8090/api/caisse';
+
+  constructor(private http:HttpClient) { }
+
+  getAllInCaisse(): Observable<Product []> {
+    return this.http.get<Product[]>(`${this.apiUrl}/list`);
+  }
+  saveOrders(orders: any[]): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/saveOrder`, orders);
+  }
+  
+  
+}
